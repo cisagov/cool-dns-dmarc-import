ad96c0ba46afc4a9db9fefc12c389d3ddd13f92d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-06 12:39:05 -0400

    Add backend and remote state, and create a lambda creation policy
    The lambda creation policy is attached to the ProvisionAccount role.

--- variables.tf
+++ variables.tf
@@ -4,26 +4,42 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)"
-}
-
-variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
-}
-
-variable "subnet_id" {
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)"
-}
-
 # ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 
+variable "aws_region" {
+  type        = string
+  description = "The AWS region to communicate with."
+  default     = "us-east-1"
+}
+
+variable "create_lambda_policy_description" {
+  type        = string
+  description = "The description to associate with the IAM policy that allows sufficient permissions to create the dmarc-import Lambda function."
+  default     = "Allows sufficient permissions to create the dmarc-import Lambda function."
+}
+
+variable "create_lambda_policy_name" {
+  type        = string
+  description = "The name to assign the IAM policy that allows sufficient permissions to create the dmarc-import Lambda function."
+  default     = "CreateDmarcImportLambda"
+}
+
+variable "lambda_name" {
+  type        = string
+  description = "The name of the dmarc-import Lambda function."
+  default     = "dmarc-import"
+}
+
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources created"
-  default     = {}
+  default = {
+    Team        = "VM Fusion - Development"
+    Application = "COOL - DNS - dmarc-import"
+    Workspace   = "production"
+  }
 }
