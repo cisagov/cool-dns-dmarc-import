62dc6eb6923ca57cebabd668a3d7a61bc5714166
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-08 09:47:30 -0400

    Add dmarc-import module and the input variables it needs

---
+++ dmarc_import.tf
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Build and configure the dmarc-import infrastructure.
+#-------------------------------------------------------------------------------
+
+module "dmarc_import" {
+  # source = "github.com/cisagov/dmarc-import-tf-module"
+  source = "../dmarc-import-tf-module"
+
+  providers = {
+    aws = aws.dnsprovisionaccount
+  }
+
+  aws_region                = var.aws_region
+  elasticsearch_domain_name = var.elasticsearch_domain_name
+  elasticsearch_index       = var.elasticsearch_index
+  elasticsearch_type        = var.elasticsearch_type
+  emails                    = var.emails
+  lambda_function_name      = var.lambda_function_name
+  lambda_function_zip_file  = var.lambda_function_zip_file
+  permanent_bucket_name     = var.permanent_bucket_name
+  queue_name                = var.queue_name
+  rule_set_name             = var.rule_set_name
+  tags                      = var.tags
+  temporary_bucket_name     = var.temporary_bucket_name
+}
--- variables.tf
+++ variables.tf
@@ -16,22 +16,72 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "create_lambda_policy_description" {
+variable "elasticsearch_domain_name" {
   type        = string
-  description = "The description to associate with the IAM policy that allows sufficient permissions to create the dmarc-import Lambda function."
-  default     = "Allows sufficient permissions to create the dmarc-import Lambda function."
+  description = "The domain name of the Elasticsearch instance."
+  default     = "dmarc-import-elasticsearch"
 }
 
-variable "create_lambda_policy_name" {
+variable "elasticsearch_index" {
   type        = string
-  description = "The name to assign the IAM policy that allows sufficient permissions to create the dmarc-import Lambda function."
-  default     = "CreateDmarcImportLambda"
+  description = "The Elasticsearch index to which to write DMARC aggregate report data."
+  default     = "dmarc_aggregate_reports"
 }
 
-variable "lambda_name" {
+variable "elasticsearch_type" {
+  type        = string
+  description = "The Elasticsearch type corresponding to a DMARC aggregate report."
+  default     = "report"
+}
+
+variable "emails" {
+  type        = list(string)
+  description = "A list of the email addresses at which DMARC aggregate reports are being received."
+  default = [
+    "reports@dmarc.cyber.dhs.gov",
+  ]
+}
+
+variable "lambda_function_name" {
   type        = string
   description = "The name of the dmarc-import Lambda function."
   default     = "dmarc-import"
+}
+
+variable "lambda_function_zip_file" {
+  type        = string
+  description = "The location of the zip file for the Lambda function."
+  default     = "../dmarc-import-lambda/dmarc-import.zip"
+}
+
+variable "permanent_bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket where the DMARC aggregate report emails are stored permanently."
+  default     = "cool-dmarc-import-permanent"
+}
+
+variable "provisiondmarcimport_policy_description" {
+  type        = string
+  description = "The description to associate with the IAM policy that allows sufficient permissions to provision the dmarc-import infrastructure."
+  default     = "Allows sufficient permissions to provision the dmarc-import infrastructure."
+}
+
+variable "provisiondmarcimport_policy_name" {
+  type        = string
+  description = "The name to assign the IAM policy that allows sufficient permissions to provision the dmarc-import infrastructure."
+  default     = "ProvisionDmarcImport"
+}
+
+variable "queue_name" {
+  type        = string
+  description = "The name of the SQS queue where events will be sent as DMARC aggregate reports are received."
+  default     = "cool-dmarc-import-queue"
+}
+
+variable "rule_set_name" {
+  type        = string
+  description = "The name of the SES rule set that processes DMARC aggregate reports."
+  default     = "dmarc-import-rules"
 }
 
 variable "tags" {
@@ -42,4 +92,10 @@ variable "tags" {
     Application = "COOL - DNS - dmarc-import"
     Workspace   = "production"
   }
+}
+
+variable "temporary_bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket where the DMARC aggregate report emails are stored temporarily (until processed)."
+  default     = "cool-dmarc-import-temporary"
 }
