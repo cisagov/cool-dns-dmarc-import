8a978c7db0f80b2bcf35af524203219dfbac22ee
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-13 10:07:07 -0400

    Add read-only and read-write roles for the ES DB

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "elasticsearchreadonly_role" {
+  value       = aws_iam_role.elasticsearchreadonly_role
+  description = "IAM role that allows sufficient permissions to read (but not write) to the dmarc-import Elasticsearch database."
+}
+
+output "elasticsearchreadwrite_role" {
+  value       = aws_iam_role.elasticsearchreadwrite_role
+  description = "IAM role that allows sufficient permissions to read and write to the dmarc-import Elasticsearch database."
+}
+
 output "provisiondmarcimport_policy" {
   value       = aws_iam_policy.provisiondmarcimport
   description = "IAM policy that allows sufficient permissions to provision the dmarc-import infrastructure."
